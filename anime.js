2b79da076b3513d699d85384a799447c91b06d9f
minechoo <choominhae@gmail.com>
2023-06-21 14:07:49 +0900

    scroll 까지

--- anime.js
+++ anime.js
@@ -1,26 +1,16 @@
-/*
-  peformance.now();
-  1ms단위로 정밀한 시간계산이 가능
-  브라우저가 로딩된 순간부터 해당 구문이 호출된 시점까지의 시간을 ms단위로 반환
-  정밀한 시간계산이 필요할때 활용됨
-
-  특정시간동안 특정수치값의 변경
-  반복횟수 X : 고정된 반복횟수안에서 변화량을 제어 O
-*/
-
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
 
 btn.addEventListener('click', () => {
-	anime(box, {
-		prop: 'margin-left',
-		value: 500,
+	anime(window, {
+		prop: 'scroll',
+		value: 1000,
 		duration: 1000,
 		callback: () => {
 			console.log('end');
 			anime(box, {
-				prop: 'margin-top',
-				value: 500,
+				prop: 'top',
+				value: '50%',
 				duration: 1000,
 			});
 		},
@@ -30,11 +20,30 @@ btn.addEventListener('click', () => {
 function anime(selector, option) {
 	const startTime = performance.now();
 	console.log('시작시간', startTime);
+	let currentValue = null;
 
-	//현재 css에 적용되어있는 값을 가져온뒤, parseInt 를 활용해 숫자값으로 변경
-	const currentValue = parseInt(getComputedStyle(selector)[option.prop]);
+	option.prop === 'scroll' ? (currentValue = selector.scrollY) : parseFloat(getComputedStyle(selector)[option.prop]);
 
-	if (option.value !== currentValue) requestAnimationFrame(run);
+	//만약에 value 속성으로 받은 값이 문자열이면 퍼센트연산처리 해야되므로 정수가 아닌 실수로 변환
+	const isString = typeof option.value;
+	if (isString === 'string') {
+		const parentW = parseInt(getComputedStyle(selector.parentElement).width);
+		const parentH = parseInt(getComputedStyle(selector.parentElement).height);
+
+		const x = ['left', 'right', 'width'];
+		const y = ['top', 'bottom', 'height'];
+		const errProps = ['margin-left', 'margin-right', 'padding-left', 'padding-right', 'margin-top', 'margin-bottom', 'padding-top', 'padding-bottom'];
+
+		for (let cond of errProps) if (option.prop === cond) return console.error('margin, padding값은 퍼센트 모션처리할 수 없습니다.');
+
+		//option.prop값으로 위의 배열로 설정한 속성이 들어오면 currentValue값을 부모요소의 크기대비 퍼센트로 변환
+		for (let cond of x) option.prop === cond && (currentValue = (currentValue / parentW) * 100);
+		for (let cond of y) option.prop === cond && (currentValue = (currentValue / parentH) * 100);
+
+		option.value = parseFloat(option.value);
+	}
+
+	option.value !== currentValue && requestAnimationFrame(run);
 
 	function run(time) {
 		let timelast = time - startTime;
@@ -47,6 +56,10 @@ function anime(selector, option) {
 		console.log('진행률', progress);
 
 		let result = currentValue + (option.value - currentValue) * progress;
-		selector.style[option.prop] = result + 'px';
+
+		if (isString === 'string') selector.style[option.prop] = result + '%';
+		else if (option.prop === 'opacity') selector.style[option.prop] = result;
+		else if (option.prop === 'scroll') selector.scroll(0, result);
+		else selector.style[option.prop] = result + 'px';
 	}
 }
